39c4e255eaf67b65052470315077fff53ee3a5bd
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-10-20 12:18:29 +0330

    feat: Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Features from "@/components/templates/index/Features";
+import Gallery from "@/components/templates/index/Gallery";
+import Home from "@/components/templates/index/Home";
+import Story from "@/components/templates/index/Story";
+import React from "react";
 
-function Home() {
+function Index() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Features />
+      <Story />
+      <Home />
+      <Gallery />
+    </>
+  );
 }
 
-export default Home
+export default Index;
